2fc8d3da88b833f4fd4132cb92896f87d87030f8
welrce <welrce@126.com>
2020-03-16 18:33:07 +0800

    * init

--- video/src/main/java/com/welrce/ape/video/MainFragment.kt
+++ video/src/main/java/com/welrce/ape/video/MainFragment.kt
@@ -1,5 +1,12 @@
 package com.welrce.ape.video
 
+import android.os.Bundle
+import com.angcyo.http.base.toJson
+import com.angcyo.jsoup.html.HtmlCategory
+import com.angcyo.jsoup.html.css.AttrSelect
+import com.angcyo.jsoup.html.css.HtmlElementSelect
+import com.angcyo.jsoup.html.parse
+import com.angcyo.library.L
 import com.welrce.ape.video.base.AppFragment
 
 /**
@@ -13,5 +20,47 @@ import com.welrce.ape.video.base.AppFragment
 class MainFragment : AppFragment() {
     init {
         fragmentLayoutId = R.layout.fragment_main
+    }
+
+    override fun onFragmentShow(bundle: Bundle?) {
+        super.onFragmentShow(bundle)
+
+        val htmlCategory = HtmlCategory().apply {
+            htmlUrl = "https://www.wo03.com/"
+            categoryName = "推荐"
+            elementsCss = "body #banner .carousel-inner .item | body #banner .carousel-text li "
+
+            htmlElementSelect = HtmlElementSelect().apply {
+
+                titleSelect = AttrSelect().apply {
+                    attrTargetCss = ".title"
+                    attrKey = AttrSelect.ATTR_KEY_TEXT
+                }
+
+                summarySelect = AttrSelect().apply {
+                    attrTargetCss = ".text"
+                    attrKey = AttrSelect.ATTR_KEY_TEXT
+                }
+
+                coverSelect = AttrSelect().apply {
+                    attrTargetCss = "a"
+                    attrKey = "style"
+                    attrPattern = listOf("(?<=url\\(').*(?=')")
+                    attrIsUrl = true
+                }
+
+                targetUrlSelect = AttrSelect().apply {
+                    attrTargetCss = "a"
+                    attrKey = "href"
+                    attrIsUrl = true
+                }
+            }
+        }
+
+        L.i(htmlCategory.toJson())
+
+        htmlCategory.parse {
+            L.i(htmlCategory.toJson())
+        }
     }
 }
